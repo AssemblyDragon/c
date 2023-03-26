f0d06529f3c783581017b9ef4ed39d107d78bcb0
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-03-26 15:59:45 +0530

    Create sumOfNNaturalNumbersRecusion.c

---
+++ sumOfNNaturalNumbersRecusion.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int sum(int n){
+    if (n == 1){
+        return 1;
+    }
+    else return (n + sum(n-1));
+}
+
+int main(){
+    int n;
+    printf("Enter Number : ");
+    scanf("%d", &n);
+    printf("Sum till n = %d natural numbers is : %d", n, sum(n));
+    return 0;
+}
