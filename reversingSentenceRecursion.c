df1f9141cb34e2a19716233c9cb70acf4180e63a
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-03-26 16:00:37 +0530

    Create reversingSentenceRecursion.c

---
+++ reversingSentenceRecursion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void reverse(){
+    char c;
+    scanf("%c", &c);
+    if (c == '\n'){
+        return;
+    }
+    reverse();
+    printf("%c", c);
+}
+
+int main(){
+    printf("Enter a Sentence : ");
+    reverse();
+    return 0;
+}
