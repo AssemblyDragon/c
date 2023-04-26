aaf54b07297b5dc4da618c771a95d41b844ee24b
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-04-26 22:29:15 +0530

    Create memoryAllocation.c

---
+++ memoryAllocation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n=5;
+    int *ptr;
+    ptr = (int *)malloc(n*sizeof(int));
+    ptr[1] = 1;
+    printf("%d", ptr[1]);
+    free(ptr);
+    return 0;
+}
