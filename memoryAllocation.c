a91d3695dbc18ff51e37d796906493f6d204f5fc
Yash Midha <72779011+AssemblyDragon@users.noreply.github.com>
2023-06-08 23:01:13 +0530

    Update memoryAllocation.c

--- memoryAllocation.c
+++ memoryAllocation.c
@@ -6,7 +6,7 @@
 int main(){
     int n=5;
     int *ptr;
-    ptr = (int *)malloc(n*sizeof(int));
+    ptr = (int *)malloc(n*sizeof(int));  // dynamically allocating memory of size 5*4 = 20
     ptr[1] = 1;
     printf("%d", ptr[1]);
     free(ptr);
